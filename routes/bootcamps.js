f403304f27bd9ac2472e3dcf29b30ad0521ff47a
mohamednasr20 <mohamednasr8687@gmail.com>
2021-02-18 15:38:19 -0500

    create bootcamps routes

---
+++ routes/bootcamps.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
+
+router.get('/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `get bootcamp ${req.params.id}` });
+});
+router.post('/', (req, res) => {
+  res.status(200).json({ success: true, msg: 'create new bootcamp' });
+});
+router.put('/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `update bootcamp ${req.params.id}` });
+});
+router.delete('/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `delete bootcamp ${req.params.id}` });
+});
+
+module.exports = router;
