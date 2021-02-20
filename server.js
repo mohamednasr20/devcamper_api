125dd2d1a2e22c78a462b63d1c1009f2046abaf2
mohamednasr20 <mohamednasr8687@gmail.com>
2021-02-19 22:55:01 -0500

    connect to DB

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const dotenv = require('dotenv');
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
 
+connectDB();
+
+const bootcamps = require('./routes/bootcamps');
+const connectDb = require('./config/db');
+
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`server running in ${process.env.NODE_ENV} mode on prot ${PORT}`);
+});
+
+process.on('undandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+
+  server.close(() => process.exit(1));
 });
