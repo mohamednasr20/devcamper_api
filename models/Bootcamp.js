bc0dfea6306553aff67fd998518b6ddfd27fc02a
mohamednasr20 <mohamednasr8687@gmail.com>
2021-02-23 00:24:44 -0500

    create goecode hook

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utilts/geocoder');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -33,6 +34,7 @@ const BootcampSchema = new mongoose.Schema({
       'Please add a valid email',
     ],
   },
+
   address: {
     type: String,
     required: [true, 'Please add an address'],
@@ -99,6 +101,23 @@ const BootcampSchema = new mongoose.Schema({
 
 BootcampSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+BootcampSchema.pre('save', async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  this.address = undefined;
   next();
 });
 
